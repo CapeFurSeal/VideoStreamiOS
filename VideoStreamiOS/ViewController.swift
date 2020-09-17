1ca8fa4398d0dc95c769b1b945f0bb83eba4b5a7
Blake Loizides <blake.loizides@hpe.com>
2020-09-17 07:24:53 +0200

    Initial Commit

---
+++ VideoStreamiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoStreamiOS
+//
+//  Created by Blake Loizides on 9/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
